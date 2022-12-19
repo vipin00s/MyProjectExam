62f516bf288674abff061d2ab242f5c145162b3f
vipising <vipising@INLEN8520060313.corp.capgemini.com>
2022-12-19 10:14:00 +0530

    first commits

--- examportal/src/main/java/com/exam/repository/QuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exam.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.exam.model.Quiz;
-
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-
-}
